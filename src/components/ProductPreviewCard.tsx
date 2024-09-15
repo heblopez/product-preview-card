42af75c7330c76321e1995dcba72cb689ca88253
Heberth López <heberth.lopez.19@gmail.com>
2024-09-15 12:10:47 -0500

    feat: Adjustments for desktop design

--- src/components/ProductPreviewCard.tsx
+++ src/components/ProductPreviewCard.tsx
@@ -1,13 +1,14 @@
 import iconCart from '../assets/icon-cart.svg'
 import productMobileImg from '../assets/image-product-mobile.jpg'
+import productDesktopImg from '../assets/image-product-desktop.jpg'
 
 export default function ProductPreviewCard() {
   return (
-    <article className='bg-white rounded-lg overflow-hidden'>
-      <figure>
-        <img src={productMobileImg} alt="product-preview" />
+    <article className='bg-white rounded-lg overflow-hidden max-w-[425px] mx-auto md:max-w-[600px] md:flex md:items-stretch md:justify-center md:gap-4'>
+      <figure className='md:self-stretch md:w-1/2'>
+        <img src={window.innerWidth >= 768 ? productDesktopImg : productMobileImg} alt="product-preview" className='md:w-full md:h-full md:object-cover' />
       </figure>
-      <div className='p-6 gap-2 flex flex-col'>
+      <div className='p-6 gap-2 flex flex-col md:w-1/2 md:self-center'>
         <p className='text-xs uppercase tracking-[0.25rem] mb-1'>Perfume</p>
         <h1 className='font-title text-title font-bold text-very-dark-blue leading-none mb-1'>Gabrielle Essence Eau De Parfum</h1>
         <p className='text-paragraph my-1'>A flora, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
